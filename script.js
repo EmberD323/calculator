31c144442431bceb93b964eee7b2040d5535af5d
Ember Doyle <doyle.ember@gmail.com>
2024-09-10 11:45:05 -0600

    Fix rounding error for operates

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function add (a,b){
     return ((a*10)+(b*10))/10;
 }
 function subtract (a,b){
-    return a-b;
+    return ((a*10)-(b*10))/10;;
 }
 function multiply (a,b){
     return ((a*10)*(b*10))/10;
 }
 function divide (a,b){
-    return a/b;
+    return ((a*10)/(b*10))/10;;
 }
 
 let firstNumber;
