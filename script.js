eefb9916452a6ba1661e8f7381ebd551bf658a29
Ember Doyle <doyle.ember@gmail.com>
2024-09-10 10:27:52 -0600

    Add click events for equals, clear and decimal

--- script.js
+++ script.js
@@ -14,12 +14,17 @@ function divide (a,b){
 let firstNumber;
 let secondNumber;
 let operator;
+let displayElement = document.querySelector(".display");
+let displayNumber = displayElement.textContent;
+console.log(displayNumber);
 
 function operate (firstNumber,operator,secondNumber){
-    if (operator == "+") return add(firstNumber,secondNumber); 
-    if (operator == "-") return subtract(firstNumber,secondNumber); 
-    if (operator == "*") return multiply(firstNumber,secondNumber); 
-    if (operator == "/") return divide(firstNumber,secondNumber); 
+    if (operator == "+") displayNumber = add(firstNumber,secondNumber); 
+    if (operator == "-") displayNumber = subtract(firstNumber,secondNumber); 
+    if (operator == "*") displayNumber = multiply(firstNumber,secondNumber); 
+    if (operator == "/") displayNumber = divide(firstNumber,secondNumber); 
+    console.log(displayNumber);
+    return displayNumber;
 }
 
 //number button event listener
@@ -27,12 +32,29 @@ let numberButtons = document.querySelectorAll(".number");
 numberButtons.forEach((button)=>{
     button.addEventListener("click",()=>{
         if (firstNumber == undefined){
-            firstNumber = button.textContent;
+            firstNumber = Number(button.textContent);
             console.log("first number is " + firstNumber);
+            displayElement.textContent = firstNumber;
         }
+        //if firstNumber defined but operator isnt, add clicked number to first number string
+        else if(firstNumber !== undefined && operator == undefined){
+            firstNumber = Number(firstNumber + button.textContent);
+            console.log("first number is " + firstNumber);
+            displayElement.textContent = firstNumber;
+            
+        }
+        
         else if(secondNumber == undefined){
-            secondNumber = button.textContent;
+            secondNumber = Number(button.textContent);
             console.log("second number is " + secondNumber);
+            displayElement.textContent = secondNumber;
+        }
+        //if firstNumber defined but operator isnt, add clicked number to first number string
+        else{
+            secondNumber = Number(secondNumber + button.textContent);
+            console.log("second number is " + secondNumber);
+            displayElement.textContent = secondNumber;
+            
         }
     });
 });
@@ -41,9 +63,63 @@ numberButtons.forEach((button)=>{
 let operatorButtons = document.querySelectorAll(".operator");
 operatorButtons.forEach((button)=>{
     button.addEventListener("click",()=>{
-        if (operator == undefined){
             operator = button.textContent;
             console.log("operator is " + operator);
-        }
     });
+});
+
+//equals button event listener
+let equals = document.querySelector(".equals");
+equals.addEventListener("click",()=>{
+    if(operator != undefined && firstNumber != undefined && firstNumber != undefined){
+        operate(firstNumber,operator,secondNumber);
+        console.log("equals " + displayNumber);
+        displayElement.textContent = displayNumber;
+        
+    }
+});
+
+//clear button event listener
+let clear = document.querySelector("#clear");
+clear.addEventListener("click",()=>{
+    displayElement.textContent = 0;
+    firstNumber = undefined;
+    secondNumber = undefined;
+    operator = undefined;
+});
+
+//decimal button event listener
+let decimal = document.querySelector("#decimal");
+decimal.addEventListener("click",()=>{
+    if(operator == undefined){//if operator undefined add to firstnumber
+        //if theres already one decimal in the number
+        if(firstNumber == undefined){
+            firstNumber = "0" + decimal.textContent
+            console.log("first number is " + firstNumber);
+            displayElement.textContent = firstNumber;
+
+        }
+        else if(firstNumber % 1 != 0){
+        }
+        else{
+            firstNumber =firstNumber + decimal.textContent;
+            console.log("first number is " + firstNumber);
+            displayElement.textContent = firstNumber;
+        }
+    }
+    else{
+        if(secondNumber == undefined){
+            secondNumber = "0" + decimal.textContent
+            console.log("second number is " + secondNumber);
+            displayElement.textContent = secondNumber;
+
+        }
+        else if(secondNumber % 1 != 0){
+        }
+        else{
+            secondNumber =secondNumber + decimal.textContent;
+            console.log("second number is " + secondNumber);
+            displayElement.textContent = secondNumber;
+        }
+    }
 });
